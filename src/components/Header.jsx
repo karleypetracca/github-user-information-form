fdd4ff8269892f6dbd15abfbc5e8df27039a3989
Karley Petracca <karleypetracca@gmail.com>
2020-03-31 18:19:31 -0400

    Refactor code to prep for adding cards

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+	return(
+		<header className="App-header">
+			<p>
+					GitHub user information form
+			</p>
+		</header>
+	)
+}
+
+export default Header;
